0fe7add037b3408a7590f80af9d889dc9b468771
KartikChavda123 <kartikchavda2005@gmail.com>
2024-05-15 17:18:06 +0530

    Control Structure - 3

---
+++ Control Structure - 3/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+
+    int a,b,c;
+
+    printf("Enter number of a:");
+    scanf("%d",&a);
+
+    printf("Enter number of b:");
+    scanf("%d",&b);
+
+    printf("Enter number of c:");
+    scanf("%d",&c);
+
+    (a<b) ? (a<c) ? printf("A is minimum"): printf("C is minimum") : (b<c) ? printf("B is minimum") : printf("C is Minimum");
+}
